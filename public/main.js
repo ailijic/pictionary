dd38319dd8149c7fb2f0a858da7a8a9735686459
Andrew Ilijic <ailijic@gmail.com>
2016-12-16 14:20:16 -0500

    Add func that returns a list of words

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 // public/main.js
-/* global $ */
+/* global $, io */
 
 start()
 function start () {
@@ -7,24 +7,65 @@ function start () {
 
   $(document).ready(() => { pictionary() })
   function pictionary () {
+    const socket = io()
     const canvas = $('canvas')
     const context = canvas[0].getContext('2d')
     canvas[0].width = canvas[0].offsetWidth
     canvas[0].height = canvas[0].offsetHeight
 
+    // Local Events
+    let drawing = false
+    $(window).on('mousedown', () => {
+      drawing = true
+    })
+    $(window).on('mouseup', () => {
+      drawing = false
+    })
     canvas.on('mousemove', (event) => {
-      const offset = canvas.offset()
-      const position = {
-        x: event.pageX - offset.left,
-        y: event.pageY - offset.top
-      }
-      draw(position)
-      function draw (position) {
-        context.beginPath()
-        context.arc(position.x, position.y,
-                      6, 0, 2 * Math.PI)
-        context.fill()
+      if (drawing) {
+        const offset = canvas.offset()
+        const position = {
+          x: event.pageX - offset.left,
+          y: event.pageY - offset.top
+        }
+        draw(position)
+
+        socket.emit('draw', position)
+      } else {
+        return
       }
     })
+
+    // Server Events
+    socket.on('guess', (guess) => {
+      $('#guessList').prepend(`<li>${guess}</li>`)
+    })
+
+    socket.on('draw', (position) => {
+      draw(position)
+    })
+
+    function draw (position) {
+      context.beginPath()
+      context.arc(position.x, position.y,
+                    6, 0, 2 * Math.PI)
+      context.fill()
+    }
+
+    const guessBox = $('#guess input')
+    guessBox.on('keydown', onKeyDown)
+    function onKeyDown (event) {
+      if (event.keyCode !== 13) { // Enter: key code 13
+        return
+      }
+
+      console.log(guessBox.val())
+      socket.emit('guess', guessBox.val())
+      guessBox.val('')
+    }
+  }
+
+  function WORDS () {
+    return ['word', 'letter', 'number', 'person', 'pen', 'class', 'people', 'sound', 'water', 'side', 'place', 'man', 'men', 'woman', 'women', 'boy', 'girl', 'year', 'day', 'week', 'month', 'name', 'sentence', 'line', 'air', 'land', 'home', 'hand', 'house', 'picture', 'animal', 'mother', 'father', 'brother', 'sister', 'world', 'head', 'page', 'country', 'question', 'answer', 'school', 'plant', 'food', 'sun', 'state', 'eye', 'city', 'tree', 'farm', 'story', 'sea', 'night', 'day', 'life', 'north', 'south', 'east', 'west', 'child', 'children', 'example', 'paper', 'music', 'river', 'car', 'foot', 'feet', 'book', 'science', 'room', 'friend', 'idea', 'fish', 'mountain', 'horse', 'watch', 'color', 'face', 'wood', 'list', 'bird', 'body', 'dog', 'family', 'song', 'door', 'product', 'wind', 'ship', 'area', 'rock', 'order', 'fire', 'problem', 'piece', 'top', 'bottom', 'king', 'space']
   }
 }
--- server.js
+++ server.js
@@ -6,13 +6,22 @@ function start () {
 
   const http = require('http')
   const express = require('express')
-  const socket_io = require('socket.io')
+  const socketIO = require('socket.io')
 
   const app = express()
   app.use(express.static('public'))
 
   const server = http.Server(app)
-  const io = socket_io(server)
+  const io = socketIO(server)
+  io.on('connection', (socket) => {
+    socket.on('draw', (position) => {
+      socket.broadcast.emit('draw', position)
+    })
+
+    socket.on('guess', (guess) => {
+      socket.broadcast.emit('guess', guess)
+    })
+  })
 
   server.listen(process.env.PORT || 8080)
 }
